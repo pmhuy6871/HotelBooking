64b3535365bec58862dc872497116ea9f0217a1c
Hyu <85073537+pmhuy6871@users.noreply.github.com>
2024-12-14 19:07:17 +0700

    Add files via upload

--- home.php
+++ home.php
@@ -1,336 +1,79 @@
-<div class="intro">
-    <div class="container">
-      <div class="row">
-        <div class="col">
-          <div class="section_title text-center">
-            <div>Welcome</div>
-            <h1>Amazing Hotel in front of the Sea</h1>
-          </div>
-        </div>
-      </div>
-      <div class="row intro_row">
-        <div class="col-xl-8 col-lg-10 offset-xl-2 offset-lg-1">
-          <div class="intro_text text-center">
-            <p>Maecenas sollicitudin tincidunt maximus. Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scelerisque at. Quisque eget sem non ligula consectetur ultrices in quis augue. Donec imperd iet leo eget tortor dictum, eget varius eros sagittis. Curabitur tempor dignissim massa ut faucibus sollicitudin tinci dunt maximus. Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scele risque at. Quisque eget sem non ligula consectetur ultrices in quis augue. Donec imperdiet leo eget tortor dictum, eget varius eros sagittis. Curabitur tempor dignissim massa ut faucibus.</p>
-          </div>
-        </div>
-      </div>
-      <div class="row gallery_row">
-        <div class="col">
+<div class="container">
+<h3>Administrator Panel:Welcome <?php echo $_SESSION['ADMIN_UNAME'];?></h3>
 
-          <!-- Gallery -->
-          <div class="gallery_slider_container">
-            <div class="owl-carousel owl-theme gallery_slider">
-              
-              <!-- Slide -->
-              <div class="gallery_slide">
-                <img src="images/gallery_1.jpg" alt="">
-                <div class="gallery_overlay">
-                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
-                    <a href="#">
-                      <!-- <span>+</span>
-                      <span>See More</span> -->
-                    </a>
-                  </div>
-                </div>
-              </div>
-
-              <!-- Slide -->
-              <div class="gallery_slide">
-                <img src="images/gallery_2.jpg" alt="">
-                <div class="gallery_overlay">
-                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
-                    <a href="#">
-                      <!-- <span>+</span>
-                      <span>See More</span> -->
-                    </a>
-                  </div>
-                </div>
-              </div>
-
-              <!-- Slide -->
-              <div class="gallery_slide">
-                <img src="images/gallery_3.jpg" alt="">
-                <div class="gallery_overlay">
-                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
-                    <a href="#">
-                      <!-- <span>+</span>
-                      <span>See More</span> -->
-                    </a>
-                  </div>
-                </div>
-              </div>
-
-              <!-- Slide -->
-              <div class="gallery_slide">
-                <img src="images/gallery_4.jpg" alt="">
-                <div class="gallery_overlay">
-                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
-                    <a href="#">
-                      <!-- <span>+</span>
-                      <span>See More</span> -->
-                    </a>
-                  </div>
-                </div>
-              </div>
-
-            </div>
-          </div>
-
-        </div>
+<div class="panel-group" id="accordion">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+          Rooms
+        </a>
+      </h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse in">
+      <div class="panel-body">
+      The guest house has got various rooms that are categorised accordion to types. 
+      Each room is of particular category and have a maximum number of Adults and Children that can be accomodated. Click<a href="mod_room/index.php"> HERE.</a>
       </div>
     </div>
   </div>
-
-  <!-- Rooms -->
-
-  <div class="rooms_right container_wrapper">
-    <div class="container">
-      <div class="row row-eq-height">
-
-        <!-- Rooms Image -->
-        <div class="col-xl-6 order-xl-1 order-2">
-          <div class="rooms_slider_container">
-            <div class="owl-carousel owl-theme rooms_slider">
-              
-              <!-- Slide -->
-              <div class="slide">
-                <div class="background_image" style="background-image:url(images/rooms_1.jpg)"></div>
-              </div>
-
-              <!-- Slide -->
-              <div class="slide">
-                <div class="background_image" style="background-image:url(images/rooms_1.jpg)"></div>
-              </div>
-
-              <!-- Slide -->
-              <div class="slide">
-                <div class="background_image" style="background-image:url(images/rooms_1.jpg)"></div>
-              </div>
-
-            </div>
-          </div>
-        </div>
-
-        <!-- Rooms Content -->
-        <div class="col-xl-6 order-xl-2 order-1">
-          <div class="rooms_right_content">
-            <div class="section_title">
-              <div>Rooms</div>
-              <h1>Luxury Double Suite</h1>
-            </div>
-            <div class="rooms_text">
-              <p>Maecenas sollicitudin tincidunt maximus. Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scelerisque at. Quisque eget sem non ligula consectetur ultrices in quis augue. Donec imperd iet leo eget tortor dictum, eget varius eros sagittis. Curabitur tempor dignissim massa ut faucibus sollicitudin tinci dunt maximus. Morbi tempus malesuada erat sed pellentesque.</p>
-            </div>
-            <div class="rooms_list">
-              <ul>
-                <li class="d-flex flex-row align-items-center justify-content-start">
-                  <img src="images/check.png" alt="">
-                  <span>Morbi tempus malesuada erat sed</span>
-                </li>
-                <li class="d-flex flex-row align-items-center justify-content-start">
-                  <img src="images/check.png" alt="">
-                  <span>Tempus malesuada erat sed</span>
-                </li>
-                <li class="d-flex flex-row align-items-center justify-content-start">
-                  <img src="images/check.png" alt="">
-                  <span>Pellentesque vel neque finibus elit</span>
-                </li>
-              </ul>
-            </div>
-            <div class="rooms_price">$129/<span>Night</span></div>
-           
-          </div>
-        </div>
-
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+          Accomodation
+        </a>
+      </h4>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse">
+      <div class="panel-body">
+        This consists of the categories of rooms that in this Hotel. Each Category of rooms Has got unique features different form the other. For view all of the categories of all types of rooms Click <a href="mod_roomtype/index.php">HERE.</a>  </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+          Reservation
+        </a>
+      </h4>
+    </div>
+    <div id="collapseThree" class="panel-collapse collapse">
+      <div class="panel-body">
+       In this area, you can view all the reservation transaction of all guest. And this area allow the the receptionist confirm the request of guest or either to cancel the reservation. Click <a href="mod_reservation/index.php">HERE.</a>
+       </div>
+    </div>
+  </div>
+ 
+   <?php if($_SESSION['ADMIN_UROLE']=="Administrator"){ ?>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapsesix">
+          Users
+        </a>
+      </h4>
+    </div>
+    <div id="collapsesix" class="panel-collapse collapse">
+      <div class="panel-body">
+		    The system displays the list of all people that have been registered in to the system.If a particular user is logged in the system the, such as users record is does not appear in the list of records. To view all the registered other than the logged in user Click <a href="mod_users/index.php">HERE.</a>
       </div>
     </div>
   </div>
-
-  <!-- Rooms -->
-
-  <div class="rooms_left container_wrapper">
-    <div class="container">
-      <div class="row row-eq-height">
-        
-        <!-- Rooms Content -->
-        <div class="col-xl-6">
-          <div class="rooms_left_content">
-            <div class="section_title">
-              <div>Rooms</div>
-              <h1>Luxury Single Room</h1>
-            </div>
-            <div class="rooms_text">
-              <p>Maecenas sollicitudin tincidunt maximus. Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scelerisque at. Quisque eget sem non ligula consectetur ultrices in quis augue. Donec imperd iet leo eget tortor dictum, eget varius eros sagittis. Curabitur tempor dignissim massa ut faucibus sollicitudin tinci dunt maximus. Morbi tempus malesuada erat sed pellentesque.</p>
-            </div>
-            <div class="rooms_list">
-              <ul>
-                <li class="d-flex flex-row align-items-center justify-content-start">
-                  <img src="images/check.png" alt="">
-                  <span>Morbi tempus malesuada erat sed</span>
-                </li>
-                <li class="d-flex flex-row align-items-center justify-content-start">
-                  <img src="images/check.png" alt="">
-                  <span>Tempus malesuada erat sed</span>
-                </li>
-                <li class="d-flex flex-row align-items-center justify-content-start">
-                  <img src="images/check.png" alt="">
-                  <span>Pellentesque vel neque finibus elit</span>
-                </li>
-              </ul>
-            </div>
-            <div class="rooms_price">$89/<span>Night</span></div>
-           
-          </div>
-        </div>
-
-        <!-- Rooms Image -->
-        <div class="col-xl-6">
-          <div class="rooms_slider_container">
-            <div class="owl-carousel owl-theme rooms_slider">
-              
-              <!-- Slide -->
-              <div class="slide">
-                <div class="background_image" style="background-image:url(images/rooms_2.jpg)"></div>
-              </div>
-
-              <!-- Slide -->
-              <div class="slide">
-                <div class="background_image" style="background-image:url(images/rooms_2.jpg)"></div>
-              </div>
-
-              <!-- Slide -->
-              <div class="slide">
-                <div class="background_image" style="background-image:url(images/rooms_2.jpg)"></div>
-              </div>
-              
-            </div>
-          </div>
-        </div>
-
+  <!-- <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseseven">
+          Utilities
+        </a>
+      </h4>
+    </div>
+    <div id="collapseseven" class="panel-collapse collapse">
+      <div class="panel-body">
+          In this area, you can change the description and slided of pictures of the home page. Click <a href="mod_settings/index.php">HERE.</a>
       </div>
     </div>
-  </div>
-
-  <!-- Discover -->
-
-  <div class="discover">
-
-    <!-- Discover Content -->
-    <div class="discover_content">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-5">
-            <div class="section_title">
-              <div>Hotel</div>
-              <h1>Discover Marimar Hotel</h1>
-            </div>
-          </div>
-        </div>
-        <div class="row discover_row">
-          <div class="col-lg-5">
-            <div class="discover_highlight">
-              <p>Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scelerisque at. Quisque eget sem non ligula consectetur.</p>
-            </div>
-            <!-- <div class="button discover_button"><a href="#">discover</a></div> -->
-          </div>
-          <div class="col-lg-7">
-            <div class="discover_text">
-              <p>Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scelerisque at. Quisque eget sem non ligula consectetur ultrices in quis augue. Donec imperd iet leo eget tortor dictum, eget varius eros sagittis. Curabitur tempor dignissim massa ut faucibus sollicitudin tinci dunt maximus. Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis. Donec pharetra mattis nulla, id laoreet neque scelerisque at. Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scelerisque at. Quisque eget sem non ligula consectetur ultrices in quis augue.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <!-- Discover Slider -->
-    <div class="discover_slider_container">
-      <div class="owl-carousel owl-theme discover_slider">
-        
-        <!-- Slide -->
-        <div class="slide">
-          <div class="background_image" style="background-image:url(images/discover_1.jpg)"></div>
-          <div class="discover_overlay d-flex flex-column align-items-center justify-content-center">
-            <h1><a href="#">Weddings</a></h1>
-          </div>
-        </div>
-
-        <!-- Slide -->
-        <div class="slide">
-          <div class="background_image" style="background-image:url(images/discover_2.jpg)"></div>
-          <div class="discover_overlay d-flex flex-column align-items-center justify-content-center">
-            <h1><a href="#">Parties</a></h1>
-          </div>
-        </div>
-
-        <!-- Slide -->
-        <div class="slide">
-          <div class="background_image" style="background-image:url(images/discover_3.jpg)"></div>
-          <div class="discover_overlay d-flex flex-column align-items-center justify-content-center">
-            <h1><a href="#">Relax</a></h1>
-          </div>
-        </div>
-
-      </div>
-    </div>
-
-  </div>
-
-  <!-- Testimonials -->
-
-  <div class="testimonials">
-    <div class="container">
-      <div class="row">
-        <div class="col">
-          <div class="section_title text-center">
-            <div>Clients</div>
-            <h1>Testimonials</h1>
-          </div>
-        </div>
-      </div>
-      <div class="row testimonials_row">
-        <div class="col">
-          
-          <!-- Testimonials Slider -->
-          <div class="testimonials_slider_container">
-            <div class="owl-carousel owl-theme testimonials_slider">
-
-              <!-- Slide -->
-              <div>
-                <div class="testimonial_text text-center">
-                  <p>Maecenas sollicitudin tincidunt maximus. Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scelerisque at. Quisque eget sem non ligula consectetur ultrices in quis augue. Donec imperd iet leo eget tortor dictum, eget varius eros sagittis. Curabitur tempor dignissim massa ut faucibus sollicitudin tinci dunt maximus. Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scele risque at. Quisque eget.</p>
-                </div>
-                <div class="testimonial_author text-center">
-                  <div class="testimonial_author_image"><img src="images/author_1.jpg" alt=""></div>
-                  <div class="testimonial_author_name"><a href="#">Maria Smith,</a><span> Client</span></div>
-                </div>
-              </div>
-
-              <!-- Slide -->
-              <div>
-                <div class="testimonial_text text-center">
-                  <p>Maecenas sollicitudin tincidunt maximus. Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scelerisque at. Quisque eget sem non ligula consectetur ultrices in quis augue. Donec imperd iet leo eget tortor dictum, eget varius eros sagittis. Curabitur tempor dignissim massa ut faucibus sollicitudin tinci dunt maximus. Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scele risque at. Quisque eget.</p>
-                </div>
-                <div class="testimonial_author text-center">
-                  <div class="testimonial_author_image"><img src="images/author_1.jpg" alt=""></div>
-                  <div class="testimonial_author_name"><a href="#">Maria Smith,</a><span> Client</span></div>
-                </div>
-              </div>
-
-              <!-- Slide -->
-              <div>
-                <div class="testimonial_text text-center">
-                  <p>Maecenas sollicitudin tincidunt maximus. Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scelerisque at. Quisque eget sem non ligula consectetur ultrices in quis augue. Donec imperd iet leo eget tortor dictum, eget varius eros sagittis. Curabitur tempor dignissim massa ut faucibus sollicitudin tinci dunt maximus. Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scele risque at. Quisque eget.</p>
-                </div>
-                <div class="testimonial_author text-center">
-                  <div class="testimonial_author_image"><img src="images/author_1.jpg" alt=""></div>
-                  <div class="testimonial_author_name"><a href="#">Maria Smith,</a><span> Client</span></div>
-                </div>
-              </div>
-
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  </div> -->
+ <?php } ?>
+</div>
+</div>
--- logout.php
+++ logout.php
@@ -1,30 +1,22 @@
 <?php
-require_once("includes/initialize.php");
+require_once("../includes/initialize.php");
 // Four steps to closing a session
 // (i.e. logging out)
 
 // 1. Find the session
-@session_start();
- 
+session_start();
+
 // 2. Unset all the session variables
-unset($_SESSION['GUESTID']);	
-unset($_SESSION['name']); 		
-unset($_SESSION['last']);	
-unset($_SESSION['country']);
-unset($_SESSION['city']); 		
-unset($_SESSION['address']); 	
-unset($_SESSION['zip']); 		
-unset($_SESSION['phone']); 	
-unset($_SESSION['email']); 		
-unset($_SESSION['pass']); 	
-unset($_SESSION['from']); 
-unset($_SESSION['to']); 	
-unset($_SESSION['dragonhouse_cart']); 	
+// unset( $_SESSION['ADMIN_ID'] );
+// unset( $_SESSION['ADMIN_UNAME'] );
+// unset( $_SESSION['ADMIN_USERNAME'] );
+// unset( $_SESSION['ADMIN_UPASS'] );
+// unset( $_SESSION['ADMIN_UROLE'] );
+foreach($_SESSION as $k => $v){
+	unset($_SESSION[$k]);
+}
 
-
-//Destroy the session
-//session_destroy();
-
-redirect(WEB_ROOT ."index.php?logout=1");
+ 	
+// 4. Destroy the session
+redirect(WEB_ROOT ."admin/index.php?logout=1");
 ?>
-
